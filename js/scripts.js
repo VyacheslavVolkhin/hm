56ed11887229fcfd984ce849609676ab03b21a84
VyacheslavVolkhin <vv@htmlworker.com>
2023-04-26 14:02:06 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,41 @@ $(document).ready(function(){
             ]
         });
     }
+
+    //main-info-box
+    if (!!$('.main-info-box').offset()) {
+        let mS = $('.main-info-box .slider').slick({
+            dots: false,
+            slidesToShow: 1,
+            variableWidth: false,
+            infinite: true,
+            adaptiveHeight: false,
+            rows: 1,
+            swipeToSlide: true,
+            prevArrow: false,
+            nextArrow: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+            ]
+        });
+        $('.main-info-box .slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+            $('.main-info-box .info-wrap li.active').removeClass('active');
+            $('.main-info-box .info-wrap li[data-slide="' + currentSlide + '"]').addClass('active');
+        });
+        $('.main-info-box .info-wrap li').click(function () {
+            let newSlide = $(this).attr('data-slide');
+            $('.main-info-box .info-wrap li').removeClass('active');
+            $(this).addClass('active');
+            $('.main-info-box .slider-wrap .slider').slick('slickGoTo', newSlide);
+            return false;
+        })
+        $('.main-info-box li[data-slide="0"]').addClass('active');
+    }
 });
-
-
